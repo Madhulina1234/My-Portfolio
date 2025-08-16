83286afbb269e700b12e900bf4b58513e53b34c0
Madhulina1234 <mahapatramadhulina05@gmail.com>
2025-08-16 23:43:48 +0530

    Initial portfolio upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const navBar = document.querySelector("nav");
+const navLinks = document.querySelector("nav");
+window.addEventListener("scroll", () => {
+  if (scrollY > 50) {
+    navBar.classList.add(
+      "bg-white",
+      "bg-opacity-50",
+      "backdrop-blur-lg",
+      "shadow-sm"
+    );
+    navLinks.classList.remove("bg-white", "bg-opacity-50", "shadow-sm");
+  } else {
+    navBar.classList.remove(
+      "bg-white",
+      "bg-opacity-50",
+      "backdrop-blur-lg",
+      "shadow-sm"
+    );
+    navLinks.classList.remove("bg-white", "bg-opacity-50", "shadow-sm");
+  }
+});
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    gridTemplateColumns: {
+      auto: "repeat(auto-FileList, minmax(200px, 1fr))",
+    },
+    animation: {
+      spin_slow: "spin 6s linear infinite",
+    },
+    colors: {
+      lightHover: "#fcf4ff",
+      darkHover: "#fcf4ff",
+    },
+  },
+};
